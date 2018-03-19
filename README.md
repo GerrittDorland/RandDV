2c2e9a2a93463cdb18bf9510d63eaa03d435816f
Gerritt <gerrittdorland@gmail.com>
2018-03-19 14:02:44 -0700

    Adds universal/library-free version.

--- README.md
+++ README.md
@@ -2,12 +2,23 @@
 
 This thing currently only works on Windows. I'll update it to be multiplatform if there is any interest.
 
-USAGE:
+USAGE (universal):
+	- While script is running, set focus to the console that is running the script.
+	- Press <ENTER>
+
+TO CLOSE (universal):
+	- Escape sequence is required. On OSX/Linux, use CTRL+C.
+	- On Windows, just close the IDLE/Powershell window.
+
+USAGE (Windows):
 	- While the script is running, press R_CONTROL. Window focus on the script is not necessary.
-	
-TO COMPILE:
+
+TO COMPILE (universal):
+	- No dependencies. Python 2.7.
+
+TO COMPILE (Windows):
 	- You need two non-standard Python libraries:
-		- win32api 
+		- win32api
 		- win32con
 	- You can get both here: https://github.com/mhammond/pywin32
 
---
+++ randdv_universal.py
@@ -0,0 +1,45 @@
+import webbrowser, time, random, urllib2
+from HTMLParser import HTMLParser
+
+doit = 0
+success = 0
+linklist = []
+
+class MyHTMLParser(HTMLParser):
+        def handle_starttag(self, tag, attrs):
+                if tag == 'a':
+                        for name, show in attrs:
+
+                                linklist.append(show)
+
+parser = MyHTMLParser()
+TLD = "http://www.distortednews.com/podshows/"
+try:
+        website = urllib2.urlopen(TLD)
+except urllib2.HTTPError:
+        print 'Tim moved the show. This script is broken until I fix it.'
+        exit(0)
+html = website.read()
+parser.feed(html)
+print 'Ready. Press R_CONTROL to request a new show.'
+
+while(1):
+        data = raw_input()
+        
+        showlink = TLD + linklist[random.randint(1,len(linklist))]
+
+        try:
+                response = urllib2.urlopen(showlink)
+                success = 1
+
+        except urllib2.HTTPError:
+                success = 0
+                print "No Show / Bad generation. Trying again..."
+
+        if success is 1:
+                success = 0
+                print "Loading show: " + showlink
+                webbrowser.open(showlink)
+
+        else:
+                time.sleep(5)

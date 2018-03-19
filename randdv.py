a77d0fb9097a0c874f1f84f6bef06a1adfe5bdb3
Gerritt <gerrittdorland@gmail.com>
2018-03-18 20:13:55 -0700

    Let's see if Developer's Swearing will find this - FUCK SHIT COCK BALLS

--- randdv.py
+++ randdv.py
@@ -1,42 +1,46 @@
 import webbrowser, win32api, win32con, time, random, urllib2
+from HTMLParser import HTMLParser
 
 doit = 0
+success = 0
+linklist = []
 
+class MyHTMLParser(HTMLParser):
+        def handle_starttag(self, tag, attrs):
+                if tag == 'a':
+                        for name, show in attrs:
+                                
+                                linklist.append(show)
+
+parser = MyHTMLParser()
+TLD = "http://www.distortednews.com/podshows/"
+website = urllib2.urlopen(TLD)
+html = website.read()
+parser.feed(html)
 
 while(1):
         if(win32api.GetAsyncKeyState(win32con.VK_RCONTROL)):
-           doit = 1
+                doit = 1
+                print 'trying...'
 
         if doit is 1:
-            doit = 0
-            m = random.randint(1, 12)
-            d = random.randint(1, 31)
-            y = random.randint(7, 18)
+                doit = 0
+                showlink = TLD + linklist[random.randint(1,len(linklist))]
+                print showlink
 
-            if y < 10:
-                stry = "0" + str(y)
-            else:
-                stry = str(y)
-            if d < 10:
-                strd = "0" + str(d)
-            else:
-                strd = str(d)
-            if m < 10:
-                strm = "0" + str(m)
-            else:
-                strm = str(m)
+                try:
+                        response = urllib2.urlopen(showlink)
+                        success = 1
+                        
+                except urllib2.HTTPError:
+                        success = 0
+                        print "No Show / Bad generation. Trying again..."
 
-            try:
-                    response = urllib2.urlopen("http://www.distortednews.com/podshows/dv_" + strm + strd + stry + "_64.mp3")
-                    
-            except urllib2.HTTPError:
-                    doit = 1
-                    print "No Show / Bad generation. Trying again..."
+                if success is 1:
+                        success = 0
+                        print "Loading show: " + showlink
+                        webbrowser.open(showlink)
 
-            if doit is 0:
-                print "Loading show: " + "dv_" + strm + strd + stry + "_64.mp3"
-                webbrowser.open("http://www.distortednews.com/podshows/dv_" + strm + strd + stry + "_64.mp3")
-
-        else:
-            time.sleep(5)
+                else:
+                        time.sleep(5)
             
